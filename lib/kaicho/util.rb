d2f2ddb1b3c17bb4d71517afda6529f4c3482563
Stone Tickle <stone.tickle@gmail.com>
2018-10-02 14:38:37 -0500

    add util module

---
+++ lib/kaicho/util.rb
@@ -0,0 +1,19 @@
+module Kaicho
+  # A utility module for Kaicho.  Don't touch this stuff!
+  module Util
+    module_function
+
+    # raise an exception if a type is invalid
+    #
+    # @param [Class] expected the expected type
+    # @param [Instance] got the received type
+    # @return [True] this method always returns true or raises an exception
+    def check_type(expected, got)
+      unless expected === got
+        raise TypeError.new("expected #{expected.name} got #{got}:#{got.class.name}")
+      end
+
+      true
+    end
+  end
+end
