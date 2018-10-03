7410398ca3294126d35ecbcaf92d265938806332
Stone Tickle <lattis@mochiro.moe>
2018-10-03 11:47:19 -0500

    change @see to links

--- lib/kaicho.rb
+++ lib/kaicho.rb
@@ -109,7 +109,7 @@ module Kaicho
   #   - +:fail+   - don't try to update this resource if the dependant is not
   #     defined
   # @param [Array] triggers a list of triggers.  These triggers must have been
-  #   previously defined using @see #add_triggers
+  #   previously defined using {#add_triggers}
   # @param [Boolean] overwrite if a resource with this name already exists,
   #   should it be overwritten
   # @param [Object, nil] share if +nil+, this resource is stored as an instance
@@ -117,9 +117,9 @@ module Kaicho
   #   is stored as a class variable owned by the class specified.
   # @param [Symbol] accessor a symbol that determines which attribute accessors
   #   should be generated for this resource
-  #   - +:read+,  +:r+  - @see #attr_reader
-  #   - +:write+, +:w+  - @see #attr_writer
-  #   - +:both+,  +:rw+ - @see #attr_accessor
+  #   - +:read+,  +:r+  - defines an {#attr_reader}
+  #   - +:write+, +:w+  - defines an {#attr_writer}
+  #   - +:both+,  +:rw+ - defines both, see {#attr_accessor}
   #   - +:none+         - don't generate any accessors
   # @param block a block that will be called, with no arguments, to update this
   #   resource
