06ddd912a3487d6c12b567957be089af92ee8721
Stone Tickle <lattis@mochiro.moe>
2018-10-03 11:36:58 -0500

    flesh out basic spec covering readme example

--- spec/kaicho_spec.rb
+++ spec/kaicho_spec.rb
@@ -1,9 +1,46 @@
-RSpec.describe Kaicho do
-  it 'has a version number' do
-    expect(Kaicho::VERSION).not_to be nil
-  end
+class Fixture
+  attr_reader :computations
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  include Kaicho
+
+  def initialize
+    def_resource(:apples, accessor: :both) { @apples || 0 }
+    def_resource(:oranges, accessor: :both) { @oranges || 0 }
+    def_resource(:total, depends: { apples: :fail, oranges: :fail }) do
+      @computations += 1
+      @apples + @oranges
+    end
+
+    @computations = 0
+  end
+end
+
+RSpec.describe Kaicho do
+  context 'fruit example' do
+
+    f = Fixture.new
+
+    it 'has accessors' do
+      expect(f).to respond_to :apples
+      expect(f).to respond_to :apples=
+      expect(f).to respond_to :oranges
+      expect(f).to respond_to :oranges=
+    end
+
+    it 'can use accessors' do
+      expect(f.apples).to eq 0
+      f.apples += 1
+      expect(f.apples).to eq 1
+
+      expect(f.oranges).to eq 0
+      f.oranges += 1
+      expect(f.oranges).to eq 1
+    end
+
+    it 'automatically updates' do
+      10.times { f.total }
+      expect(f.total).to eq 2
+      expect(f.computations).to eq 2
+    end
   end
 end
